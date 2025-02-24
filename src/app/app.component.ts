ea9f958ff6c1a0bdffa7d40ac362dd7e2aff0a0c
Shereen-Esther <shereenjubal@gmail.com>
2025-02-24 11:03:17 -0500

    Added sample component to app component to check changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BachelorsComponent } from './bachelors/bachelors.component';
+import { SampleComponent } from './sample/sample.component';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [BachelorsComponent],
+  imports: [SampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
